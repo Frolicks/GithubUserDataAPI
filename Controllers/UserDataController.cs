4372d57dca4d2c863daa0d4f73aa0982de366535
Frolicks <dr.frolicks@gmail.com>
2022-05-15 02:47:29 -0700

    added UserDataController

---
+++ Controllers/UserDataController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GithubUserDataAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserDataController : ControllerBase
+    {
+
+        [HttpGet("{username}")]
+        public IActionResult GetUserData(string username)
+        {
+            return Ok("Hello" + username); 
+        }
+    }
+}
